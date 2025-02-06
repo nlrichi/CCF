473cf6a9a6e4b62b41066b7650bb139e6dfc40bc
nlrichi <129979689+nlrichi@users.noreply.github.com>
2025-02-06 01:57:28 +0000

    Delete Pyactivity1.py

--- Pyactivity1.py
+++
@@ -1,9 +0,0 @@
-import random
-Userinput = int(input("Guess my number between 1 and 10"))
-snum = random.randint(1,10)
-print(snum)
-if Userinput == snum:
-    print("You got it right")
-else:
-    print("you're nearly there")
-
